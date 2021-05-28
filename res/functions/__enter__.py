99896ba461b7c7d5372c6638e12f793bb997dd59
GaelicGrime <will.angus.blaylock@gmail.com>
2021-05-28 00:39:24 -0600

    Updated __enter__.py.

--- res/functions/__enter__.py
+++ res/functions/__enter__.py
@@ -5,5 +5,5 @@
 		self._MAINFRAME_ = SG.Window(**self._WINDOW_).finalize()
 		ALL_THE_FORMS[self._THIS_FORM_NAME_] = self._MAINFRAME_
 		self.init()
-		return self
+		# return self
 		# fold here ⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3
