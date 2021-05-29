c0b5e78201098b3f3c7c17958d1c68a42b6f0ab0
GaelicGrime <will.angus.blaylock@gmail.com>
2021-05-28 18:28:57 -0600

    Updated enint.py.

--- res/functions/easyUpdate.py
+++ res/functions/easyUpdate.py
@@ -1,42 +1,50 @@
 	def easyUpdate(self,
-			CHECKBOX_ALPHA_DIM_=None,
-			CHECKBOX_RUNAWAY_=None,
-			EVENTMODE_=None,
-			INTERVAL_COUNT_=None,
-			NAME_NEXT_EVENT_=None,
-			TIME_AT_NEXT_=None,
-			TIME_AT_ZEROELAPSE_=None,
-			TIME_CLOCK_=None,
-			TIME_ELAPSED_=None,
-			TIME_TOGO_=None,
+			checkboxAlphaDim_=None,
+			checkboxRunaway_=None,
+			eventmode_=None,
+			intervalCount_=None,
+			nameNextEvent_=None,
+			timeAtNext_=None,
+			timeAtZeroelapse_=None,
+			timeClock_=None,
+			timeElapsed_=None,
+			timeTogo_=None,
 		):
 		# fold here ⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3
-		if NAME_NEXT_EVENT_ is not None:
-			self._DICTIN_[NAME_NEXT_EVENT] = NAME_NEXT_EVENT_
+		if nameNextEvent_ is not None:
+			self._DICTIN_[NAME_NEXT_EVENT] = nameNextEvent_
 
-		if CHECKBOX_ALPHA_DIM_ is not None:
-			self._DICTIN_[CHECKBOX_ALPHA_DIM] = CHECKBOX_ALPHA_DIM_
+		if checkboxAlphaDim_ is not None:
+			self._DICTIN_[CHECKBOX_ALPHA_DIM] = checkboxAlphaDim_
 
-		if CHECKBOX_RUNAWAY_ is not None:
-			self._DICTIN_[CHECKBOX_RUNAWAY] = CHECKBOX_RUNAWAY_
+		if checkboxRunaway_ is not None:
+			self._DICTIN_[CHECKBOX_RUNAWAY] = checkboxRunaway_
 
-		if INTERVAL_COUNT_ is not None:
-			self._DICTIN_[INTERVAL_COUNT] = INTERVAL_COUNT_
+		if eventmode_ is not None:
+			self.CURRENT_EVENTMODE = eventmode_
 
-		if TIME_AT_NEXT_ is not None:
-			self._DICTIN_[TIME_AT_NEXT] = TIME_AT_NEXT_
+			if intervalCount_ is not None:
+				self._DICTIN_[INTERVAL_COUNT] = intervalCount_
 
-		if TIME_AT_ZEROELAPSE_ is not None:
-			self._DICTIN_[TIME_AT_ZEROELAPSE] = TIME_AT_ZEROELAPSE_
+			if (eventmode_ == EVENTMODE_INTERVAL):
+				self.intervalCountOn()
+			else:
+				self.intervalCountOff()
 
-		if TIME_CLOCK_ is not None:
-			self._DICTIN_[TIME_CLOCK] = TIME_CLOCK_
+		if time_at_next_ is not None:
+			self._DICTIN_[TIME_AT_NEXT] = time_at_next_
 
-		if TIME_ELAPSED_ is not None:
-			self._DICTIN_[TIME_ELAPSED] = TIME_ELAPSED_
+		if timeAtZeroelapse_ is not None:
+			self._DICTIN_[TIME_AT_ZEROELAPSE] = timeAtZeroelapse_
 
-		if TIME_TOGO_ is not None:
-			self._DICTIN_[TIME_TOGO] = TIME_TOGO_
+		if timeClock_ is not None:
+			self._DICTIN_[TIME_CLOCK] = timeClock_
+
+		if timeElapsed_ is not None:
+			self._DICTIN_[TIME_ELAPSED] = timeElapsed_
+
+		if timeTogo_ is not None:
+			self._DICTIN_[TIME_TOGO] = timeTogo_
 
 		self.enint()
 		self.updateFromDict()
