b4a67f1cbe22994acbd906d986a41c434829a3f3
GaelicGrime <will.angus.blaylock@gmail.com>
2021-05-28 20:44:48 -0600

    Updated intervalCountOn.py.

--- PSG_TIME.py
+++ PSG_TIME.py
@@ -1254,7 +1254,7 @@ class CLASS_CLOCKS(object):
 
 	def intervalCountOn(self):
 		global \
-			ALL_THE_FORMS, \
+			ALL_THE_FORMS
 		# fold here ⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1
 		ALL_THE_FORMS[self._THIS_FORM_NAME_][INTERVAL_COUNT].update(text_color=COLOR_TIME_TOGO)
 		self.updateFromDict()
