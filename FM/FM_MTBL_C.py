f97011bb9f4afc3d1c7c3ec502e70ecc11cf3fa1
GaelicGrime <will.angus.blaylock@gmail.com>
2021-06-16 16:29:52 -0600

    Updated FM_TBGLST_PSG_CLOCKS.py.

--- FM/FM_MTBL_C.py
+++ FM/FM_MTBL_C.py
@@ -12,10 +12,10 @@ class MTBL_C():
 
 		_moduleName_ = "FM"
 		_sub0_ = "TBGLST"
-		_newFilename_ = FM.NAME_LCL_SUB_NEW_PY(_moduleName_, _sub0_)
-		_filename_ = FM.NAME_LCL_SUB_PY(_moduleName_, _sub0_)
+		_newFilename_ = FM.NAME_LCL_SUB0_NEW_PY(_moduleName_, _sub0_)
+		_filename_ = FM.NAME_LCL_SUB0_PY(_moduleName_, _sub0_)
 
-		_IDToRtn_ = self.makeID(moduleName_=_moduleName_, sub0_=_sub0_, filename_=_newFilename_)
+		_IDToRtn_ = self.makeID(moduleName=_moduleName_, sub0=_sub0_, filename=_newFilename_)
 
 		_TBGLSTEntry_ = (
 			"FM_TBGLST",
@@ -33,12 +33,22 @@ class MTBL_C():
 
 	def makeID(self, **KWargs_):
 		# fold here ⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2
+
 		_class_ = KWargs_.get("class")
 		_filename_ = KWargs_.get("filename")
 		_moduleName_ = KWargs_.get("moduleName")
 		_sub0_ = KWargs_.get("sub0")
 		_sub1_ = KWargs_.get("sub1")
 		_sub2_ = KWargs_.get("sub2")
+
+		print(f"""KWargs_ {KWargs_}
+_class_ {_class_} = KWargs_.get("class")
+_filename_ {_filename_} = KWargs_.get("filename")
+_moduleName_ {_moduleName_} = KWargs_.get("moduleName")
+_sub0_ {_sub0_} = KWargs_.get("sub0")
+_sub1_ {_sub1_} = KWargs_.get("sub1")
+_sub2_ {_sub2_} = KWargs_.get("sub2")
+""")
 
 		_IDToRtn_ = (
 			("class", _class_,),
@@ -49,6 +59,7 @@ class MTBL_C():
 			("sub2", _sub2_,),
 		)
 
+		print(f"""_IDToRtn_ {_IDToRtn_}""")
 		return _IDToRtn_
 		# fold here ⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2
 
@@ -65,15 +76,16 @@ class MTBL_C():
 			_thisFD_.close()
 		# fold here ⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2
 
-	def startLCLNewFile(self, **KWargs_):
+	def startLCLNewFile(self, IDIn_):
 		# fold here ⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2⥥2
 		_strToRtn_ = ""
-		_class_ = KWargs_.get("class")
-		_filename_ = KWargs_.get("filename")
-		_moduleName_ = KWargs_.get("moduleName")
-		_sub0_ = KWargs_.get("sub0")
-		_sub1_ = KWargs_.get("sub1")
-		_sub2_ = KWargs_.get("sub2")
+		print(f"""IDIn_ {IDIn_}""")
+		_class_ = IDIn_.get("class")
+		_filename_ = IDIn_.get("filename")
+		_moduleName_ = IDIn_.get("moduleName")
+		_sub0_ = IDIn_.get("sub0")
+		_sub1_ = IDIn_.get("sub1")
+		_sub2_ = IDIn_.get("sub2")
 
 			# __MODULE_NAME__, _NEW
 			# 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥
@@ -137,8 +149,8 @@ TBGLST = {FM.OBRKT}
 				(_sub2_ is None) \
 			):
 			# fold here ⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3⥥3
-			_filename_ = f"""{FM.NAME_LCL_SUB0_PY(_moduleName_, _class_)}"""
-			_newFilename_ = f"""{FM.NAME_LCL_SUB0_PY_NEW_PY(_moduleName_)}"""
+			_filename_ = f"""{FM.NAME_LCL_SUB0_PY(_moduleName_, _sub0_)}"""
+			_newFilename_ = f"""{FM.NAME_LCL_SUB0_NEW_PY(_moduleName_, _sub0_)}"""
 			_strToRtn_ += f"""{FM.NEWLINE}{FM.NEWLINE}from FM import FM{FM.NEWLINE}{FM.NEWLINE}
 __MODULE_NAME__ = {FM.DBLQT}{_moduleName_}{FM.DBLQT}
 __SUB0__ = {FM.DBLQT}{_sub0_}{FM.DBLQT}
@@ -150,7 +162,8 @@ __ID__ = {FM.OPAREN}
 {FM.CPAREN}{FM.NEWLINE}
 TBGLST = {FM.OBRKT}
 """
-			self.__FILE_DICT__[_filename_].write(_strToRtn_)
+			print(f"""__FILE_DICT__ {self.__FILE_DICT__}""")
+			self.__FILE_DICT__[_newFilename_].write(_strToRtn_)
 			# fold here ⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3⥣3
 		# ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2
 
@@ -164,12 +177,20 @@ TBGLST = {FM.OBRKT}
 
 		_IDBeingExploded_ = itemToExplode_[1]
 		_itemToExplode_ = itemToExplode_[0]
-		print(f"""itemToExplode_ {itemToExplode_}""")
-		_ID_ = dict((x, y) for x, y in _itemToExplode_)
+		print(f"""_IDBeingExploded_ {_IDBeingExploded_}
+_itemToExplode_ {_itemToExplode_}""")
+		_ID_ = dict((x, y) for x, y in _IDBeingExploded_)
 
 			# 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥
 		if (_ID_["filename"] is not None) and (_ID_["filename"] not in self.__FILE_DICT__):
 			self.__FILE_DICT__[_ID_["filename"]] = open(_ID_["filename"], "tw")
+			_filename_ = f"""{_ID_["filename"]}"""
+			print(f"""itemToExplode_ {itemToExplode_}
+_ID_ {_ID_}
+_ID_[filename] |{_ID_["filename"]}|
+_filename_ {_filename_}
+""")
+
 			self.startLCLNewFile(_ID_)
 		# ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2
 
@@ -185,7 +206,7 @@ TBGLST = {FM.OBRKT}
 		_strToWrt_ = _strToWrt_[:-1] + f"""{FM.CPAREN},{FM.NEWLINE}"""
 		_strToWrt1_ = _strToWrt1_[:-1] + f"""{FM.CPAREN},{FM.NEWLINE}"""
 		self.__FILE_DICT__[_ID_["filename"]].write(_strToWrt_)
-		self.__FILE_DICT__[FM.NAME_LCL_SUB_NEW_PY("FM", "TBGLST")].write(_strToWrt1_)
+		self.__FILE_DICT__[FM.NAME_LCL_SUB0_NEW_PY("FM", "TBGLST")].write(_strToWrt1_)
 
 		return _ID_, itemToExplode_
 	# fold here ⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2⥣2
---
+++ FM/FM_TBGLST_NEW.py
@@ -0,0 +1,49 @@
+
+
+from FM import FM
+
+
+__MODULE_NAME__ = "FM"
+__SUB0__ = "TBGLST"
+__FILENAME__ = f"""{FM.NAME_LCL_CLASS_PY(__MODULE_NAME__, __CLASS__)}"""  # FM/FM_TBGLST.py
+__ID__ = (
+	("moduleName", __MODULE_NAME__,),  # FM
+	("filename", __FILENAME__,),  # FM/FM_TBGLST.py
+	("sub0", __SUB0__,),  # TBGLST
+)
+
+TBGLST = [
+	({'class': None, 'filename': 'FM/FM_TBGLST_NEW.py', 'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': None, 'sub2': None}, "('FM_TBGLST', 'FMAX_NOP', 'start of all things list')", FM.(('class', None), ('filename', 'FM/FM_TBGLST_NEW.py'), ('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', None), ('sub2', None)),),
+	({'class': None, 'filename': 'FM/FM_TBGLST_NEW.py', 'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': None, 'sub2': None}, "('FM_TBGLST', 'FMAX_NOP', 'start of all things list')", FM.(('class', None), ('filename', 'FM/FM_TBGLST_NEW.py'), ('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', None), ('sub2', None)),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('CFVAL', 'FMAX_NOP', 'CFVAL_BEGINS')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('CFVAL_____', 'FMAX_NOP', 'CFVAL_ENDS')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF', 'FMAX_NOP', 'FMAXCF_BEGINS')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0003_LAMBDA_DEF', 'FMAXFM_SCTN0101_AX_DEF', 'define a lambda function <NAC><K_EVENT_NAME><lambda str>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0003_TYPE_DEF', 'FMAXFM_SCTN0101_AX_DEF', 'define a fake type used in the translation dict <NAC><K_EVENT_NAME><TYPE>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0201_STR_DEF', 'FMAXFM_SCTN0101_AX_DEF', 'define a STR in SCTN0201 <NAC><K_EVENT_NAME><str>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0201_VAL_DEF', 'FMAXFM_SCTN0101_AX_DEF', 'define a VAL in SCTN0201 <NAC><K_EVENT_NAME><VAL>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0202_OPTIONSDICT_STR_ADD', 'FMAXFM_SCTN0101_AX_DEF', "define a OPTNAME: 'str' in SCTN0202 <NAC><KEY><PARM><STRDEFAULT>")", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0202_OPTIONSDICT_VAL_ADD', 'FMAXFM_SCTN0101_AX_DEF', 'define a OPTNAME: VAL in SCTN0202 <NAC><KEY><PARM><VALDEFAULT>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0202_OPTIONS_ADD_HELP_LINE', 'FMAXFM_SCTN0101_AX_DEF', 'add a line to the help for a PARM, after the comment is automatically added<NAC><PARM><ADDONHELPLINE>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0202_OPTIONS_STR_ADD', 'FMAXFM_SCTN0101_AX_DEF', "define a '-a[=]' in SCTN22 <NAC><PARM><KEY><VAL>")", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0202_OPTIONS_VAL_ADD', 'FMAXFM_SCTN0101_AX_DEF', "define a '-a[=]' in SCTN22 <NAC><PARM><KEY><VAL>")", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0203_DICT_DEF', 'FMAXFM_SCTN0101_AX_DEF', 'define a dict in SCTN203 <NAC><DICTNAME><DICTMODE>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0203_DICT_VS_ADD', 'FMAXFM_SCTN0101_AX_DEF', 'define a dict STR in SCTN203 <NAC><DICTNAME><STR>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0203_DICT_VV_ADD', 'FMAXFM_SCTN0101_AX_DEF', 'define a dict VAL in SCTN203 <NAC><DICTNAME><VAL>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0204_LIST_DEF', 'FMAXFM_SCTN0101_AX_DEF', 'define a list in SCTN204 <NAC><LISTNAME>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0204_LIST_STR_ADD', 'FMAXFM_SCTN0101_AX_DEF', 'define a list STR in SCTN204 <NAC><LISTNAME><STR>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_SCTN0204_LIST_VAL_ADD', 'FMAXFM_SCTN0101_AX_DEF', 'define a VAL in a list in SCTN204 <NAC><LISTNAME><VAL>')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+	({'moduleName': 'FM', 'sub0': 'TBGLST', 'sub1': 'CF', 'filename': 'FM/FM_TBGLST_CF.py'}, "('FMAXCF_____', 'FMAX_NOP', 'FMAXCF_ENDS')", FM.(('moduleName', 'FM'), ('sub0', 'TBGLST'), ('sub1', 'CF'), ('filename', 'FM/FM_TBGLST_CF.py')),),
+]
+
+
+#
+#
+# * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+# * end of FM/FM_TBGLST_NEW.py
+# * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+#
+#
+
+
+#
