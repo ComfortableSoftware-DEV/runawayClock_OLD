7a174e2cb5e012b348651c2d6fdef100a2f76eaf
GaelicGrime <will.angus.blaylock@gmail.com>
2021-06-29 21:23:36 -0600

    Updated _02_FM.py.

--- FM/MAKE_D/_02_FM.py
+++ FM/MAKE_D/_02_FM.py
@@ -20,7 +20,17 @@ from FM import FM
 def __make__():
 	# fold here ⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1⥥1
 	# 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥ 1⥥
-	with (open(f"""{N.MAKE_NAME(__MODULE_NAME__="FM")}""", "tw") as _FDmakeFM_):
+	with (
+			(open(f"""{N.MAKE_NAME(
+					__MODULE_NAME__="FM",
+			)}""", "tw") as _FDmakeFM_)
+			(open(f"""{N.MAKE_NAME(
+					__MODULE_NAME__="__KEYS__",
+					__SUB0__="_02_",
+					__SUB1__="FM",
+					__SUB2__="AX"
+			)}""", "tw") as _FDmakeFMAX_)
+	):
 		_FDmakeFM_.write(f"""{V.readFileToStr(V.NAME_GLBL_SELF_TOP_PY("FM"))}""")
 		_FDmakeFM_.write(f"""{M.makeAComment("SCTN003 TYPEs and lambda")}""")
 		_dictToUse_ = FM.sortADict(FM.FMCF_SCTN0003_TYPE_DICT)
@@ -35,10 +45,10 @@ def __make__():
 		_dictToUse_ = FM.sortADict(FM.FMFM_SCTN0101_AX_DICT)
 		# 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥ 2⥥
 		for _thisName_, _value_ in _dictToUse_.items():
-			_FDmakeFM_.write(f"""{_thisName_} = {_value_}  # {FM.FMFM_SCTN0101_AX_CMNT_DICT[_thisName_]}{V.NEWLINE}""")
+			_FDmakeFMAX_.write(f"""{_thisName_} = {_value_}  # {FM.FMFM_SCTN0101_AX_CMNT_DICT[_thisName_]}{V.NEWLINE}""")
 			strToRtn01_ += f"""{V.NTAB(1)}{_thisName_},  # {FM.FMFM_SCTN0101_AX_CMNT_DICT[_thisName_]}{V.NEWLINE}"""
 		# ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2 ⥣2
-		_FDmakeFM_.write(f"""{V.NEWLINE}{V.NEWLINE}FMAXFM_AXLST = {V.OBRKT}{V.NEWLINE}{strToRtn01_}{V.CBRKT}{V.NEWLINE}{V.NEWLINE}{V.NEWLINE}""")
+		_FDmakeFMAX_.write(f"""{V.NEWLINE}{V.NEWLINE}FMAXFM_AXLST = {V.OBRKT}{V.NEWLINE}{strToRtn01_}{V.CBRKT}{V.NEWLINE}{V.NEWLINE}{V.NEWLINE}""")
 		_FDmakeFM_.write(f"""{M.makeAComment("SCTN102 VAL _DEF_")}""")
 		strToRtn01_ = ""
 		_dictToUse_ = FM.sortADict(FM.FMFM_SCTN0102_VAL_DICT)
