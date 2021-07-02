9cf0783f30d81dbfcb9808e04462f1464d871d37
GaelicGrime <will.angus.blaylock@gmail.com>
2021-07-02 01:38:21 -0600

    Updated _01_00_NAMES.py.

--- __KEYS__/CF/_01_00_NAMES.py
+++ __KEYS__/CF/_01_00_NAMES.py
@@ -3,7 +3,7 @@
 #
 #
 # * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
-# * start of __KEYs__.CF._01_00_NAMES
+# * start of __KEYS__.CF._01_00_NAMES
 # * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
 #
 #
@@ -29,115 +29,117 @@ __SUBD1__ = "__SUBD1__"
 __SUBSECTION__ = "__SUBSECTION__"
 
 
-# LCL LCL_KWARGS LCL_ORDER_LIST
-# fold here ⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1
+GLBL_ROOT = "/rcr/0-units/python/"
+LCL_ROOT = ""
 
-LCL_KWARGS = {
-		__CLASS__: None,
-		__EXTENSION__: "py",
-		__MODULE_NAME__: None,
-		__SUB0__: None,
-		__SUB1__: None,
-		__SUB2__: None,
-		__SUB3__: None,
-		__SUB4__: None,
-		__SUB5__: None,
-		__SUBD0__: None,
-		__SUBD1__: None,
+# LCL_ROOT_DICT LCL_ORDER_ROOT_LIST
+# fold here ⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1
+LCL_ROOT_DICT = {
+	__CLASS__: "None",
+	__CLASSES__: "None",
+	__DOCS__: "None",
+	__EXTENSION__: "None",
+	__FUNCTIONS__: "None",
+	__IMAGES__: "None",
+	__KEYS__: "None",
+	__MODULE_NAME__: "None",
+	__SECTION__: "None",
+	__SUB0__: "None",
+	__SUB1__: "None",
+	__SUB2__: "None",
+	__SUB3__: "None",
+	__SUB4__: "None",
+	__SUB5__: "None",
+	__SUBD0__: "None",
+	__SUBD1__: "None",
+	__SUBSECTION__: "None",
 }
 
-LCL_ORDER_LIST = [
-		__MODULE_NAME__: None,
-		__SUBD0__: None,
-		__SUBD1__: None,
-		__SUB0__: None,
-		__SUB1__: None,
-		__SUB2__: None,
-		__SUB3__: None,
-		__SUB4__: None,
-		__SUB5__: None,
-		__CLASS__: None,
-		__EXTENSION__: "py",
+LCL_ORDER_ROOT_LIST = [
+	__SECTION__,
+	__SUBSECTION__,
+	__MODULE_NAME__,
+	__SUBD0__,
+	__SUBD1__,
+	__CLASSES__,
+	__DOCS__,
+	__FUNCTIONS__,
+	__IMAGES__,
+	__KEYS__,
+	__SUB0__,
+	__SUB1__,
+	__SUB2__,
+	__SUB3__,
+	__SUB4__,
+	__SUB5__,
+	__CLASS__,
 ]
 # fold here ⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1
 
 
-# __KEYS__ LCL_KEYS LCL_KEYS_ORDER_LIST
+# GLBL_ROOT_DICT GLBL_ORDER_ROOT_LIST
 # fold here ⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1⟱1
-LCL_KWARGS_KEYS = {
-		__SECTION__: "__KEYS__",
-		__SUB0__: None,
-		__SUB1__: None,
-		__SUB2__: None,
-		__SUB3__: None,
-		__SUB4__: None,
-		__SUB5__: None,
-		__SUBD0__: None,
-		__SUBD1__: None,
-		__SUBSECTION__: None,
+GLBL_ROOT_DICT = {
+	__CLASS__: "None",
+	__CLASSES__: "None",
+	__DOCS__: "None",
+	__EXTENSION__: "None",
+	__FUNCTIONS__: "None",
+	__IMAGES__: "None",
+	__KEYS__: "None",
+	__MODULE_NAME__: "None",
+	__SECTION__: "None",
+	__SUB0__: "None",
+	__SUB1__: "None",
+	__SUB2__: "None",
+	__SUB3__: "None",
+	__SUB4__: "None",
+	__SUB5__: "None",
+	__SUBD0__: "None",
+	__SUBD1__: "None",
+	__SUBSECTION__: "None",
 }
 
-LCL_KEYS_ORDER_LIST = [
-		__SUB0__,
-		__SUB1__,
-		__SUB2__,
-		__SUB3__,
-		__SUB4__,
-		__SUB5__,
+GLBL_ORDER_ROOT_LIST = [
+	__SECTION__,
+	__SUBSECTION__,
+	__MODULE_NAME__,
+	__SUBD0__,
+	__SUBD1__,
+	__CLASSES__,
+	__DOCS__,
+	__FUNCTIONS__,
+	__IMAGES__,
+	__KEYS__,
+	__SUB0__,
+	__SUB1__,
+	__SUB2__,
+	__SUB3__,
+	__SUB4__,
+	__SUB5__,
+	__CLASS__,
 ]
 # fold here ⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1⟰1
-
-LCL_KWARGS_ROOT__ = {
-		__CLASSES__ = "__CLASSES__"
-		__DOCS__ = "__DOCS__"
-		__EXTENSION__ = "__EXTENSION__"
-		__FUNCTIONS__ = "__FUNCTIONS__"
-		__IMAGES__ = "__IMAGES__"
-		__KEYS__ = "__KEYS__"
-		__SECTION__: None,
-		__SUB0__: None,
-		__SUB1__: None,
-		__SUB2__: None,
-		__SUB3__: None,
-		__SUB4__: None,
-		__SUB5__: None,
-		__SUBD0__: None,
-		__SUBD1__: None,
-		__SUBSECTION__: None,
-}
-
-
-GLBL_ORDER_LIST = [
-		__SECTION__,
-		__SUBSECTION__,
-		__MODULE_NAME__,
-		__SUBD0__,
-		__SUBD1__,
-		__SUB0__,
-		__SUB1__,
-		__SUB2__,
-		__SUB3__,
-		__SUB4__,
-		__SUB5__,
-		__CLASS__,
-]
-
-
 
 
 SUBD_LIST = [
 		__MODULE_NAME__,
+		__CLASSES__,
+		__DOCS__,
+		__FUNCTIONS__,
+		__IMAGES__,
+		__KEYS__
+		__SECTION__,
+		__SUBSECTION__,
 		__SUBD0__,
 		__SUBD1__,
 ]
-
-
 
 
 #
 #
 # * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
-# * end of __KEYs__.CF._01_00_NAMES
+# * end of __KEYS__.CF._01_00_NAMES
 # * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
 #
 #
