3cda758c91845a065cef9fcab331563749513589
GaelicGrime <will.angus.blaylock@gmail.com>
2021-07-05 04:32:46 -0600

    Updated __init__.py.

--- CF/CONST_D/__init__.py
+++ CF/CONST_D/__init__.py
@@ -8,9 +8,7 @@ __all__ = [
 
 __author__ = "Liam A. Blaylock"
 __copyright__ = "BSD"
+__date__ = "2021.07.05 04:00:00-0600"
 __license__ = "GPL"
 __name__ = "CONST_D"
 __version__ = "0.7.10"
-__date__ = "2021.07.05 04:00:00-0600"
-
-\b__(all|bases|class|debug|dict|doc|file|members|metaclass|methods|module|name|slots|weakref)__\b
