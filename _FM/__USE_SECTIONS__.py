f178bc6b22774808da97d0dcedf02043bbbe4566
GaelicGrime <will.angus.blaylock@gmail.com>
2021-08-09 03:45:27 -0600

    Updated config.py.

---
+++ _FM/__USE_SECTIONS__.py
@@ -0,0 +1,43 @@
+__USE_SCTNS__ = {
+		"_00": {  # /.
+				"_00": False,  # /.
+		},
+		"_01": {  # CF (common functions)
+				"_00": False,  # CF./ (common functions)
+		},
+		"_02": {  # FM (framework maker)
+				"_00": False,  # FM (framework maker)
+				"_01": False,  # FM TBGLST
+				"_02": False,  # FM MAKE
+				"_03": False,  # FM PARSE
+		},
+		"_03": {
+				"_00": False,  # FO (file operations)
+		},
+		"_04": {  # DO (device operations)
+				"_00": False,  # DO./ (device operations)
+				"_01": False,  # DO.HBI (hot beef injector for creating keypresses etc.)
+		},
+		"_05": {
+				"_00": False,  # ??
+		},
+		"_06": {
+				"_00": False,  # SP (subprocess)
+		},
+		"_07": {  # DB (database)
+				"_00": False,  # DB./
+				"_01": False,  # DB.sqlite
+				"_02": False,  # DB.mariaDB via mysql
+				"_03": False,  # DB.mariaDB via mariaDB connector
+				"_04": False,  # DB.flatFile
+		},
+		"_08": {  # WEB.py
+				"_00": False,  # WEB.py ./
+				"_01": False,  # earthquakeSucker
+		},
+		"_09": {  # PSG
+				"_00": False,  # PSG./
+				"_01": False,  # PSG.runawayClock
+				"_FF": False,  # PSG_C
+		},
+}
